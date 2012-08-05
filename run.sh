1c8a14990dc7d1575e8774f84bf10502696feeee
Andrea Stagi <stagi.andrea@gmail.com>
2012-08-05 15:10:56 +0200

    Launch Turpial directly from script

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 $ANDROID_SDK/platform-tools/adb install -r ./bin/TurpialActivity-debug.apk
+$ANDROID_SDK/platform-tools/adb shell am start -n org.turpial.mobile/org.turpial.mobile.TurpialActivity
