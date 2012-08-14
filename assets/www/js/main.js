78a346f9373fab3a7f79026893c0c93263222cbf
Andrea Stagi <stagi.andrea@gmail.com>
2012-08-14 12:53:23 +0200

    Repo cleand

--- assets/www/js/main.js
+++
@@ -1,9 +0,0 @@
-
-
-function init() {
-    document.addEventListener("deviceready", onDeviceReady, false);
-}
-
-function onDeviceReady () {
-    new Media('/android_asset/www/sounds/cambur_pinton.ogg').play();
-}
